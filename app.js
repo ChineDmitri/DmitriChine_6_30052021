0c596be6bbd140cdfe31e6c7fbf3788ab0e9615d
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-06-05 11:41:08 +0200

    Init server et install dependencies

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const { dirname } = require('path');
+
+mongoose.connect('mongodb+srv://DmitriChine:@zertyS0Pekocko@cluster0.y1idj.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTION');
+    next();
+});
+
+app.use(bodyParser.json());
+
+module.exports = app;
