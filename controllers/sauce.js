008053c0dc6c4353777ec2332896b2a5aba79e10
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-06-09 22:59:02 +0200

    Effectue push et pull userId dans les tableaux usersLiked et userDisliked

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -60,3 +60,85 @@ exports.deleteSauce = (req, res, next) => {
         .then(() => res.status(200).json({ message: "Sauce deleted"}))
         .catch((error) => res.status(400).json({ error }));
 }
+
+exports.voteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+
+            switch(req.body.like) {
+                case 0: // si c'est 0 donc user n'est neutre, donc null par son userID!
+                    if (sauce.usersDisliked.includes(req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $pull: { usersDisliked: req.body.userId },
+                            _id: req.params.id
+                        })
+                        .then(() => res.status(200).json({ message: 'like 0' }))
+                        .catch((error) => res.status(400).json({ error }));
+                    } else {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $pull: { usersLiked: req.body.userId },
+                            _id: req.params.id
+                        })
+                            .then(() => res.status(200).json({ message: 'like 0' }))
+                            .catch((error) => res.status(400).json({ error }));
+                    }
+
+
+                    // sauceObjet.userliked.splice(indexOf(req.params.user), 1)
+                    // sauceObjet.usersDisliked.splice(indexOf(req.params.user), 1)
+                    
+                    // sauceObjet.likes = sauceObjet.userliked.length;
+                    // sauceObjet.usersDisliked = sauce.usersDisliked.length;
+                    console.log("0")
+
+
+                    break;
+                case -1:
+
+                    if (sauce.usersLiked.includes(req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $pull: { usersLiked: req.body.userId },
+                            _id: req.params.id
+                        })
+                        .then(() => res.status(200).json({ message: 'like 0' }))
+                        .catch((error) => res.status(400).json({ error }));
+                    }
+                
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $push: { usersDisliked: req.body.userId },
+                        _id: req.params.id
+                    })
+                        .then(() => res.status(200).json({ message: 'like -1' }))
+                        .catch((error) => res.status(400).json({ error }));
+
+                    
+                    
+                    console.log("-1")
+
+                    break;
+                case 1:
+
+                    if (sauce.usersDisliked.includes(req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $pull: { usersDisliked: req.body.userId },
+                            _id: req.params.id
+                        })
+                        .then(() => res.status(200).json({ message: 'like 0' }))
+                        .catch((error) => res.status(400).json({ error }));
+                    }
+                
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $push: { usersLiked: req.body.userId },
+                        _id: req.params.id
+                    })
+                        .then(() => res.status(200).json({ message: 'like -1' }))
+                        .catch((error) => res.status(400).json({ error }));
+        
+                    console.log("1")
+
+                    break;
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+    
+}
--- routes/sauce.js
+++ routes/sauce.js
@@ -10,6 +10,6 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
-// router.delete('/:id/like', auth, sauceCtrl.voteSauce);
+router.post('/:id/like', auth, sauceCtrl.voteSauce);
 
 module.exports = router;
